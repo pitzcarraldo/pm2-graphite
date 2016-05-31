7014769495b24f6af01e655c1b7d4cf418398ddc
MinkyuCho(Alan) <mrnoname@coupang.com>
2016-05-31 17:23:44 +0900

    Fix default interval

--- src/Metrics.js
+++ src/Metrics.js
@@ -1,12 +1,15 @@
+import flatten from 'flat';
+
 export default class Metrics {
   constructor() {
     this.metrics = [];
   }
 
   push(id, metrics) {
-    this.metrics.push(Object.keys(metrics).reduce((prevMetrics, metricsKey) => {
+    const flat = flatten(metrics);
+    this.metrics.push(Object.keys(flat).reduce((prevMetrics, metricsKey) => {
       prevMetrics[metricsKey.replace('$id', id)] = {
-        value: metrics[metricsKey],
+        value: flat[metricsKey],
         timestamp: Date.now()
       };
       return prevMetrics;
